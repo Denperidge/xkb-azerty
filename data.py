be432e02d56533f96384db3eaea6e94ff6ab5beb
Denperidge <cat@denperidge.com>
2026-02-20 21:50:14 +0100

    keycode fetching, all.json

--- data.py
+++ data.py
@@ -3,15 +3,19 @@ from subprocess import run
 from glob import glob
 from pathlib import Path
 from re import findall, match, RegexFlag, finditer
+from json import dumps
 
 UPSTREAM = "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git";
 DIR_XKEYBOARD_CONFIG = Path("xkeyboard-config")
 DIR_XKEYBOARD_CONFIG_SYMBOLS = DIR_XKEYBOARD_CONFIG.joinpath("symbols/")
+DIR_DATA = Path("data/")
 
 #REGEX_SYMBOLS_ENTRY = r'(?P<default>default)?.*?$\n^xkb_symbols.*?"(?P<id>.*?)".*? *{$(?P<content>(\n|.)*?)^ *?};$'
 REGEX_SYMBOLS_ENTRY = r'(?P<default>default)?.*?($\n^)?xkb_symbols.*?"(?P<id>.*?)".*?( |\n)*?{$(?P<content>(\n|.)*?)^ *?};$'
 REGEX_SYMBOLS_ENTRY_ONE_LINE = r'(?P<default>default)?.*?($\n^)?xkb_symbols.*?"(?P<id>.*?)".*?{(?P<content>.*?) *?};$'
 REGEX_INCLUDES = r'include "(?P<include>.*?)"'
+REGEX_ENTRY_NAME = r'name\[Group\d*?\] *?= *?"(?P<name>.*?)";'
+REGEX_ENTRY_KEYS = r'key <(?P<keycode>.*?)>\s*?{\s*?\[\s*?(?P<keys>.*?)\s*]'
 
 # These are not detected by the regex method, but instead manually added
 AZERTY_STYLE_LAYOUTS = [
@@ -55,12 +59,17 @@ def build_regex(detectors=AZERTY_DETECTORS):
     print(f"Final regex: {regex}")
     return regex
 
+def skip(id_str):
+    return id_str.startswith("kpdl") \
+        or id_str.startswith("level")
+
+
 def super_include(content, include_from):
     includes = findall(REGEX_INCLUDES, content)
     print(f"Includes for {symbol_id}: {includes}")
     for include in includes:
         # TODO: skip data that is currently not used & would require extra implementation
-        if include.startswith("kpdl") or include.startswith("level"):
+        if skip(include):
             print("Skipping keypad behaviour include")
             continue
 
@@ -87,7 +96,7 @@ if __name__ == "__main__":
     for symbol_file in symbol_files:
         # Determine if file should be skipped
         filename = symbol_file.relative_to(DIR_XKEYBOARD_CONFIG_SYMBOLS)
-        if symbol_file.is_dir():
+        if symbol_file.is_dir() or skip(str(filename)):
             continue
         #elif filename in AZERTY_STYLE_LAYOUTS:
         #    print(f"Skipping known exception {filename}")
@@ -104,7 +113,8 @@ if __name__ == "__main__":
         3. Lowest priority; symbols that include imports from within the same file
     """
 
-    processed_xkb_entries = dict()
+    processed_all = dict()
+    processed_azerty = dict()
     """
     for filename in [
         "latin",
@@ -116,18 +126,43 @@ if __name__ == "__main__":
     # Iterate over all symbol contents
     for symbol_id in xkb_entries:
         # Include the includes. Minimize the maximize or whatever
-        symbol_content = super_include(xkb_entries[symbol_id], symbol_id)
+        print("-----")
+        print("Parsing " + symbol_id)
+        symbol_content = xkb_entries[symbol_id]
+        symbol_content_included = super_include(symbol_content, symbol_id)
+
+        entry = {"id": symbol_id}
+
+        entry_name = list(finditer(REGEX_ENTRY_NAME, symbol_content))
+        print("\t> Fetching entry name")
+        if entry_name:
+            print("\tFound entry name")
+            entry["name"] = entry_name[0].group("name")
+        print("> Fetching entry keys")
+        entry_keys = list(finditer(REGEX_ENTRY_KEYS, symbol_content_included))
+        entry["keys"] = dict()
+
+        for key_match in entry_keys:
+            keys = key_match.group("keys").replace(" ", "").replace("\t", "").split(",")
+            if len(keys) < 1:
+                raise IndexError("Keys too small")
+            if keys[0] == "" and len(keys) == 1:
+                continue
+            entry["keys"][key_match.group("keycode")] = keys
+
+        processed_all[symbol_id] = entry
+
+        continue
+        matches = findall(regex, entry_content)
+        if len(matches) > 0:
+            print(f"Azerty detected in {symbol_file}:")
+            for match in matches:
+                print(f"\t- {match[0]}")
+            print("-----")
+        elif "azerty" in entry_content.lower():
+            print(f"Didn't detect Azerty using regex, but {entry_id} contains the string 'azerty'. Is the Regex failing? (Path: {symbol_file})")
+            print("-----")
+        
         print("---")
-
-
-    """
-            matches = findall(regex, entry_content)
-            if len(matches) > 0:
-                print(f"Azerty detected in {symbol_file}:")
-                for match in matches:
-                    print(f"\t- {match[0]}")
-                print("-----")
-            elif "azerty" in entry_content.lower():
-                print(f"Didn't detect Azerty using regex, but {entry_id} contains the string 'azerty'. Is the Regex failing? (Path: {symbol_file})")
-                print("-----")
-        """
+    DIR_DATA.joinpath("all.json").write_text(dumps(processed_all))
+    

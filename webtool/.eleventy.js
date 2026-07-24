816ce154f2847d309c22fca65390a23f0ae0020d
Denperidge <cat@denperidge.com>
2026-07-24 23:06:29 +0200

    index -> recipe, README.md index

--- webtool/.eleventy.js
+++ webtool/.eleventy.js
@@ -1,7 +1,7 @@
 import pugPlugin from "@11ty/eleventy-plugin-pug";
 import eleventyAutoCacheBuster from "eleventy-auto-cache-buster";
+import { readFileSync, readdirSync } from "node:fs";
 
-//import data from "../data/all.min.json" with {type: "json"};
 export const config = {
     dir: {
         input: "src",
@@ -9,17 +9,27 @@ export const config = {
     }
 };
 
+
 export default function (eleventyConfig) {
     eleventyConfig.addPlugin(pugPlugin);
     eleventyConfig.addPlugin(eleventyAutoCacheBuster);
+
+    eleventyConfig.addTemplate(
+        "index.md",
+        readFileSync("../README.md", {encoding: "utf-8"}),
+        {
+            layout: "layout.pug",
+        }
+    )
 
     eleventyConfig.addPassthroughCopy("src/static/");
     eleventyConfig.addPassthroughCopy({
         "../data/all.min.json": "all.min.json",
         "../data/azerty-all.min.json": "azerty-all.min.json",
         "../data/keysyms.min.json": "keysyms.min.json",
-        "node_modules/mvp.css/mvp.css": "static/mvp.css"
+        "node_modules/mvp.css/mvp.css": "static/mvp.css",
+        "../data/": "data"
     })
 
-    //eleventyConfig.addGlobalData("layouts", data)
+    eleventyConfig.addGlobalData("datafiles", readdirSync("../data/"))
 }
